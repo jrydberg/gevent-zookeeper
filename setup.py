d541836c37f35533c92d75ec2ac9b20f28a03843
Johan Rydberg <johan.rydberg@gmail.com>
2012-04-12 16:56:25 +0200

    add setup.py and manifest template.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gevent-zookeeper',
+      version='0.0',
+      description='Zookeeper bindings for gevent',
+      author='Johan Rydberg',
+      author_email='johan.rydberg@gmail.com',
+      url='https://github.com/jrydberg/gevent-zookeeper',
+      packages=['gevent_zookeeper']
+     )
